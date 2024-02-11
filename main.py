fc819d4023d187a5af149fef5c55906baf017b37
Miki2531 <mikiyasshemelis46@gmail.com>
2024-02-11 20:46:48 +0300

    updating virtual assistant

--- main.py
+++ main.py
@@ -1,4 +1,3 @@
-import requests
 import tkinter as tk
 from function.online_file import (
     find_my_api, search_on_google, search_on_wikipedia,
@@ -13,6 +12,8 @@ from datetime import datetime
 from random import choice
 import threading
 from utils import opening_text 
+from pprint import pprint
+import requests
 
 
 USERNAME = config("USER")
@@ -22,12 +23,8 @@ BOTNAME = config("BOTNAME")
 class Assistant:
 
     def __init__(self):
-        self.engine = self.pyttsx3.init('sapi5')
-
-        # set rate
+        self.engine = pyttsx3.init('sapi5')
         self.engine.setProperty('rate', 190)
-
-        # set volume
         self.engine.setProperty('volume', 1.0)
 
         # set Voice (Female)
@@ -36,7 +33,7 @@ class Assistant:
 
         self.root = tk.Tk()
         self.root.title('Voice Assistant')
-        self.label = tk.Lable(text = "BOTNAME", font=("Aerial", 120, "bold"))
+        self.label = tk.Label(text="🤖", font=("Aerial", 120, "bold"))
         self.label.pack()
         
         threading.Thread(target=self.run_assistant).start()
@@ -52,11 +49,11 @@ class Assistant:
     def greeting_user(self):
         """"Greeting the user accoriding to the hours."""
         hour = datetime.now().hour
-        if (hour>=6) and (hour < 11):
+        if 6 <= hour < 11 :
             self.talk(f"Good Morinig {USERNAME}")
-        elif(hour >=11) and (hour < 5):
+        elif 11 <=hour < 17:
             self.talk(f"Good Afternoon {USERNAME}")
-        elif(hour >= 5 ) and (hour < 9):
+        elif 17 <= hour < 21:
             self.talk(f"Good Evening {USERNAME}")
         self.talk(f"I am {BOTNAME}.  How may I assist you.")
 
@@ -72,13 +69,13 @@ class Assistant:
 
         try:
             print('Recognizing...')
-            query = r.recognize_google_cloud(audio, language = "en-US")
+            query = r.recognize_google(audio, language = "en-US")
             print(f"User said: {query}\n")
             if not 'exit' in query or 'stop' in query:
                 self.talk(choice(opening_text))
             else:
                 hour = datetime.now().hour
-                if hour >=11 and hour < 6:
+                if 21 <= hour or hour < 6:
                     self.talk("Good Night sir, Have a nice dream!")
                 else:
                     self.talk('Have a good day sir!')
@@ -89,11 +86,7 @@ class Assistant:
             return "None"
         
         return query
-    if __name__ == '__main__':
-        greeting_user()
-        query = run_assistant().lower()
-
-
+    def process_query(self, query):
         if 'open command prompt' in query or 'open cmd' in query:
             open_cmd()
         elif 'open camera' in query:
@@ -104,38 +97,42 @@ class Assistant:
 
         elif 'ip address' in query:
             ip_address = find_my_api()
-            talk(f"Your IP address is {ip_address}")
+            self.talk(f"Your IP address is {ip_address}")
 
         elif 'wikipedia' in query:
-            talk('What do you want to search on wikipedia, sir?')
-            search_query = run_assistant().lower()
+            self.talk('What do you want to search on wikipedia, sir?')
+            search_query = self.run_assistant().lower()
             results = search_on_wikipedia(search_query)
-            talk(f"According to wikipedia, {results}")
-            talk(f"For your comfort, I am priniting it on the screen sir.")
-            print(results)
+            self.talk(f"According to wikipedia, {results}")
+            self.talk(f"For your comfort, I am priniting it on the screen sir.")
+            pprint(results)
 
         elif 'youtube' in query:
-            talk('What do you want on youtube to play, sir?')
-            video = run_assistant().lower()
+            self.talk('What do you want on youtube to play, sir?')
+            video = self.run_assistant().lower()
             play_on_youtube(video)
 
         elif 'search on google' in query:
-            talk('What do you want to serach on google, sir?')
-            query = run_assistant().lower()
+            self.talk('What do you want to serach on google, sir?')
+            query = self.run_assistant().lower()
             search_on_google(query)
 
         elif 'joke' in query:
-            talk(f"I hope, you like this one, sir.")
+            self.talk(f"I hope, you like this one, sir.")
             joke = get_random_jock()
-            talk(joke)
-            talk('For your comfort, I am priniting on the screen sir.')
-            print(joke)
+            self.talk(joke)
+            self.talk('For your comfort, I am priniting on the screen sir.')
+            pprint(joke)
 
         elif "advice" in query:
-            talk(f"Here's an advice for you, sir")
+            self.talk(f"Here's an advice for you, sir")
             advice = get_random_advice()
-            talk(advice)
-            talk("For your convenience, I am printing it on the screen sir.")
-            print(advice)
+            self.talk(advice)
+            self.talk("For your convenience, I am printing it on the screen sir.")
+            pprint(advice)
 
-Assistant()
+if __name__ == '__main__':
+    assistant = Assistant()
+    assistant.greeting_user()
+    query = assistant.run_assistant().lower()
+    assistant.process_query(query)
